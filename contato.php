04ac681444d5294248abf68fb5cfd45a0a23e35c
Mateus Mendonça <mateusmendoncadr@gmail.com>
2020-06-22 22:52:28 -0300

    ajustando contato

--- contato.php
+++ contato.php
@@ -46,60 +46,64 @@
 
 		<h2 class="contato title">Contato</h2>
 
-		<div class="form-contato container">
-			<form method="POST">
-				<div class="input-box">
-					<label for="nome">Nome*</label>
-					<input type="text" name="nome" id="nome" required>
-					<div class="clear"></div>
-				</div>
-				<div class="input-box">
-					<label for="email">Email*</label>
-					<input type="email" name="email" id="email" required>
-					<div class="clear"></div>
-				</div>
-				<div class="input-box">
-					<label for="observacoes">Observações</label>
-					<textarea name="observacoes" id="observacoes"></textarea>
-					<div class="clear"></div>
-				</div>
-				<div class="clear"></div>
-				<span class="form-desc"> Campos obrigatórios *</span>
-				<div class="input-box">
-					<input type="submit" value="Enviar">
-				</div>
-			</form>
-		</div>
+		<div class="contato-box container">
 
-		<div class="contato-info container">
-			<div class="contato-left">
-				<div class="contato-item">
-					<h3 class="contato subtitle">Email</h3>
-					<a href="mailto:contato@physisjr.com">
-					<img class="contato" src="./media/gmail.svg">
-					<span class="contato">contato@physisjr.com</span>
-					</a>
-				</div>
-				<div class="contato-item">
-					<h3 class="contato subtitle">Telefone</h3>
-					<a href="tel:+5531994107552">
-						<img class="contato" src="./media/phone.svg">
-						<span class="contato">(31) 9 9410-7552</span>
-					</a>
-				</div>
-				<div class="contato-item">
-					<h3 class="contato subtitle">WhatsApp</h3>
-					<a href="https://wa.me/5531994107552">
-						<img class="contato" src="./media/whatsapp.svg">
-						<span class="contato">(31) 9 9410-7552</span>
-					</a>
-				</div>
-			</div>
-			<div class="contato-right">
-				<div class="contato-mapa">
-					<iframe id="mapa" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1849.8563982098842!2d-47.88440981655645!3d-21.983983239482097!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94b870e0750b98b3%3A0x836ca2811701c06d!2sPhysis%20Jr!5e0!3m2!1spt-BR!2sbr!4v1591991766483!5m2!1spt-BR!2sbr" width="100%" height="100%" frameborder="0" style="border:0;" allowfullscreen="0" aria-hidden="false" tabindex="0"></iframe>
+			<div class="contato-box-left">
+				<div class="contato-info">
+					<div class="contato-item">
+						<h3 class="contato subtitle">Email</h3>
+						<a href="mailto:contato@physisjr.com">
+						<img class="contato" src="./media/gmail.svg">
+						<span class="contato">contato@physisjr.com</span>
+						</a>
+					</div>
+					<div class="contato-item">
+						<h3 class="contato subtitle">Telefone</h3>
+						<a href="tel:+5531994107552">
+							<img class="contato" src="./media/phone.svg">
+							<span class="contato">(31) 9 9410-7552</span>
+						</a>
+					</div>
+					<div class="contato-item">
+						<h3 class="contato subtitle">WhatsApp</h3>
+						<a href="https://wa.me/5531994107552">
+							<img class="contato" src="./media/whatsapp.svg">
+							<span class="contato">(31) 9 9410-7552</span>
+						</a>
+					</div>
+						
 				</div>
 			</div>
+
+			<div class="contato-box-right">
+				<div class="form-contato">
+					<form method="POST">
+						<div class="input-box">
+							<label for="nome">Nome*</label>
+							<input type="text" name="nome" id="nome" required>
+						</div>
+						<div class="clear"></div>
+						<div class="input-box">
+							<label for="email">Email*</label>
+							<input type="email" name="email" id="email" required>
+						</div>
+						<div class="input-box">
+							<label for="observacoes">Observações*</label>
+							<textarea name="observacoes" id="observacoes" required></textarea>
+						</div>
+						<div class="input-box">
+							<span class="form-desc"> Campos obrigatórios *</span>
+						</div>
+						<div class="input-box">
+							<input type="submit" value="Mande seu problema!">
+						</div>
+					</form>
+				</div>
+			</div>
+			<div class="clear"></div>
+		</div>
+		<div class="contato-mapa">
+			<iframe id="mapa" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1849.8563982098842!2d-47.88440981655645!3d-21.983983239482097!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94b870e0750b98b3%3A0x836ca2811701c06d!2sPhysis%20Jr!5e0!3m2!1spt-BR!2sbr!4v1591991766483!5m2!1spt-BR!2sbr" width="100%" height="300px" frameborder="0" style="border:0;" allowfullscreen="0" aria-hidden="false" tabindex="0"></iframe>
 		</div>		
 	</div>
 
